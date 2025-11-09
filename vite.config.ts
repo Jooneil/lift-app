4755f145a73574be41a48379ac938bd239cc96d1
Josh <you@example.com>
2025-11-08 22:26:33 -0500

    build(vite): dedupe react and react-dom to prevent invalid hook call (#310)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    dedupe: ['react','react-dom'],
     alias: {
       '@supabase/auth-js/dist/module/lib/types': path.resolve(__dirname, 'src/shims/supabase-types.js'),
       '@supabase/auth-js/dist/module/lib/web3/ethereum': path.resolve(__dirname, 'src/shims/empty.js'),
