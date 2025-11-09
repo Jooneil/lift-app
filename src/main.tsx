5702aab99174be181d67a0f35d55026e548a0140
Josh <you@example.com>
2025-11-08 23:18:27 -0500

    test: render minimal OK root to isolate React 310

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div style={{ padding: 16, color: '#0ff', fontFamily: 'system-ui, sans-serif' }}>OK</div>
   </StrictMode>,
 )
